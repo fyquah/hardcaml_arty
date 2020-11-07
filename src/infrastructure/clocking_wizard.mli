ec7dab153bd963bed9766380c347ac70382661a3
Stanislav <quah.fy95@gmail.com>
2020-11-07 11:04:59 +0000

    Add documentation about output port widths

--- src/infrastructure/clocking_wizard.mli
+++ src/infrastructure/clocking_wizard.mli
@@ -11,9 +11,12 @@ end
 module O : sig
   type 'a t =
     { locked : 'a
-    ; clk_out1 : 'a
-    ; clk_out2 : 'a
-    ; clk_out3 : 'a
+    ; (** 166.6667 MHz clock *)
+      clk_out1 : 'a
+    ; (** 200MHz clock *) 
+      clk_out2 : 'a
+    ; (** 25MHz clock that can be fed into a ethernet Macphy. *)
+      clk_out3 : 'a
     }
   [@@deriving sexp_of, hardcaml]
 end
