f5621f1a4d6c34a94915cd14ed8e5cf2e9c9d380
Stanislav <quah.fy95@gmail.com>
2020-11-07 22:05:11 +0000

    Report bus skew

--- examples/blinker/route.tcl
+++ examples/blinker/route.tcl
@@ -7,5 +7,6 @@ route_design
 
 write_checkpoint -force $output_dir/post_route
 report_timing_summary -file $output_dir/post_route_timing_summary.rpt
+report_bus_skew -file $output_dir/post_route_bus_skew.rpt
 report_utilization -file $output_dir/post_route_utilization.rpt
 write_bitstream  -force $output_dir/hardcaml_arty_top.bit
