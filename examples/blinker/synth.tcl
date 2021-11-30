ac86637a96cf763ba86d6639a16677a20b7d7b80
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-11-30 01:07:23 -0500

    Add ip customization file for ethernet mac
    This was missing, and caused builds to fail

--- examples/blinker/synth.tcl
+++ examples/blinker/synth.tcl
@@ -5,10 +5,12 @@ set_property board_part digilentinc.com:arty-a7-35:part0:1.0 [current_project]
 set output_dir "./outputs"
 
 read_ip      -verbose ips/design_1_clk_wiz_0_0/design_1_clk_wiz_0_0.xci
+read_ip      -verbose ips/hardcaml_arty_tri_mode_ethernet_mac_0/hardcaml_arty_tri_mode_ethernet_mac_0.xci
 read_verilog hardcaml_arty_top.v
 
 upgrade_ip [get_ips]
 set_property generate_synth_checkpoint false [get_files ips/design_1_clk_wiz_0_0/design_1_clk_wiz_0_0.xci]
+set_property generate_synth_checkpoint false [get_files ips/hardcaml_arty_tri_mode_ethernet_mac_0/hardcaml_arty_tri_mode_ethernet_mac_0.xci]
 generate_target all [get_ips]
 validate_ip [get_ips]
 
