1882d6b0dc76b2eb9d2dcd4298302c00a362b364
eliquinox <eliquinox@outlook.com>
2022-10-30 15:07:53 +0000

    support for Arty A7 100 board

--- examples/blinker/synth.tcl
+++ examples/blinker/synth.tcl
@@ -1,7 +1,25 @@
 set_param board.repoPaths "boards/"
 
-create_project -in_memory -part xc7a35ticsg324-1L
-set_property board_part digilentinc.com:arty-a7-35:part0:1.0 [current_project]
+set board [lindex $argv 0]
+
+switch $board {
+    "arty-a7-35" {
+        puts "Setting up project for Arty A7 35"
+        create_project -in_memory -part xc7a35ticsg324-1L
+        set_property board_part digilentinc.com:arty-a7-35:part0:1.0 [current_project]
+    }
+    "arty-a7-100" {
+        puts "Setting up project for Arty A7 100"
+        create_project -in_memory -part xc7a100tcsg324-1
+        set_property board_part digilentinc.com:arty-a7-100:part0:1.0 [current_project]
+    }
+    default {
+        puts "Provide valid BOARD parameter. E.g.: BOARD={arty-a7-35|arty-a7-100}"
+        exit 1
+    }
+}
+
+
 set output_dir "./outputs"
 
 exec rm -rf outputs/ips
